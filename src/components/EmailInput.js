115392fd090861ebfc59289992b554b3f91be5f1
antmrhdqn <antmrhdqn@gmail.com>
2024-07-01 19:33:37 +0900

    Feat: 이메일 입력창 disabled 추가

--- src/components/EmailInput.js
+++ src/components/EmailInput.js
@@ -1,9 +1,9 @@
-const EmailInput = ({ email, onChange, isValid }) => {
+const EmailInput = ({ email, onChange, disabled = false }) => {
     return (
         <div className="form-group mt-4" style={{ width: 300 }}>
             <label style={{ fontWeight: "bold" }}>이메일 주소</label>
             <input
-                type="email" className="form-control  mt-2" value={email} onChange={onChange} placeholder="이메일을 입력해주세요"
+                type="email" className="form-control  mt-2" value={email} onChange={onChange} placeholder="이메일을 입력해주세요" disabled={disabled}
             />
         </div>
     );
