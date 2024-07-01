06e7b946a34517b290de2db65052e99e31ef83b1
antmrhdqn <antmrhdqn@gmail.com>
2024-07-01 19:32:58 +0900

    Feat: 저장 화면 추가

---
+++ src/page/Save.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import LocationSelect from "../components/LocationSelect";
+import TimeSelect from "../components/TimeSelect";
+import EmailInput from "../components/EmailInput";
+import axios from 'axios';
+import '../css/Main.css';
+import Button from "../components/Button";
+
+function Save() {
+    const [email, setEmail] = useState('');
+    const [selectedLocation, setSelectedLocation] = useState('');
+    const [selectedTime, setSelectedTime] = useState('');
+    const navigate = useNavigate();
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handleLocationSelect = (locationId) => {
+        setSelectedLocation(locationId);
+    };
+
+    const handleTimeSelect = (hour) => {
+        setSelectedTime(hour);
+    };
+
+    const handleSave = async () => {
+        try {
+            const jsonData = {
+                email,
+                location: selectedLocation,
+                time: selectedTime
+            }
+
+            const response = await axios.post('/v1/weather-mappings', jsonData);
+
+            console.log(response.data)
+        } catch (error) {
+            console.log('저장 실패', error)
+        }
+    };
+
+    const handleCancel = () => {
+        navigate('/');
+    };
+
+    const mainStyle = {
+        height: '100vh',
+        width: '100vw',
+        backgroundImage: 'url(/images/blue.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    };
+
+    return (
+        <div className="main" style={mainStyle}>
+            <div className="content">
+                <h1 className="mt-4 mb-4">Weather-Alarm</h1>
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                    <EmailInput email={email} onChange={handleEmailChange} />
+                    <LocationSelect onSelect={handleLocationSelect} />
+                    <TimeSelect onSelect={handleTimeSelect} />
+                    <div className="form-group" style={{ width: 300 }}>
+                        <div className="d-flex justify-content-between mt-4 mb-4">
+                            <Button label={'저장'} onClick={handleSave} className={"btn-primary"} />
+                            <Button label={'취소'} onClick={handleCancel} className={"btn-secondary"} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Save;
