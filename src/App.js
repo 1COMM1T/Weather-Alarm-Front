9d9c30c99e8a5cd9bccc4bffaa3c2fda9a52dc73
antmrhdqn <antmrhdqn@gmail.com>
2024-07-01 19:34:01 +0900

    Feat: 저장 페이지, 수정 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./page/Main";
+import Update from "./page/Update";
+import Save from "./page/Save";
 
 function App() {
 
@@ -7,6 +9,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<Main />} />
+        <Route path="/" element={<Main />} />
+        <Route path="update" element={<Update />} />
+        <Route path="save" element={<Save />} />
       </Routes>
     </BrowserRouter>
   );
